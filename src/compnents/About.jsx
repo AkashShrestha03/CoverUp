6a881e2c198f52171e4234b54f1c6888815f1934
Akash Shrestha <akashshrestha0399@gmail.com>
2024-04-20 23:28:18 +0530

    responsive

--- src/compnents/About.jsx
+++ src/compnents/About.jsx
@@ -1,27 +1,30 @@
-
-import { FiPhoneCall } from "react-icons/fi";
+import { FiMail, FiPhoneCall } from "react-icons/fi";
 
 const About = () => {
   return (
-    <center>
-      <div className="about_container">
-        <h1>Why Us?</h1>
-        <p className="fs-6 w-50 mt-5 ">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos, modi
-          eveniet tempora repudiandae eius perferendis ea, voluptate, autem
-          saepe corporis maiores omnis excepturi laudantium ipsam distinctio
-          sunt placeat sit nisi?
-        </p>
-        <div className="d-flex flex-column justify-content-center align-items-center gap-2 my-5">
+    <div className="about_container text-center ">
+      <div>
+        <div className="detail my-5 mx-5">
+          <h1>Why Us?</h1>
+          <p className="fs-6 mx-5 mt-5 text-center ">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos, modi
+            eveniet tempora repudiandae eius perferendis ea, voluptate, autem
+            saepe corporis maiores omnis excepturi laudantium ipsam distinctio
+            sunt placeat sit nisi?
+          </p>
+
           <span className="contact text-start">
             <FiPhoneCall /> +91- 9876543210
           </span>
-          <span className="contact mx-5 ">
-            <FiPhoneCall /> abc@gmail.com
+          <span className="contact mx-5  ">
+            <FiMail /> abc@gmail.com
           </span>
         </div>
       </div>
-    </center>
+        <div className="about_img">
+          <img src="img/Banner.jpg" alt="" />
+        </div>
+    </div>
   );
 };
 
